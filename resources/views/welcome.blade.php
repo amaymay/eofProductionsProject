4abb8537eba6a9fd1c4433352f933ec43b5f7001
Amy Wong <wong.amy@live.com>
2017-06-05 02:47:49 +0000

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+  <title>PhotoCom</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  
+  <!-- Custom css for the mainPage.html -->
+  <link rel="stylesheet" href="/css/mainPage.css">
+  
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+<!-- Container element -->
+<div class="parallax"></div> 
+
+    <nav class="navbar navbar-default navbar-fixed-top">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="#">PhotoCom</a>
+            </div>
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="#">Home</a></li>
+                    <li><a href="#">Page 1</a></li>
+                    <li><a href="#">Page 2</a></li>
+                    <li><a href="#">Page 3</a></li>
+                </ul>
+        </div>
+    </nav>
+    <style>
+    .parallax {
+        /* The image used */
+        background-image: url("https://img.alwayswanderlust.com/2016/04/lens-1209823_1280.jpg");
+    
+        /* Set a specific height */
+        height: 350px;
+    
+        /* Create the parallax scrolling effect */
+        background-attachment: fixed;
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: cover;
+    }
+    </style>
+    <!-- Container element -->
+    <div class="parallax">
+        <center><p style="font-size:200px; font-style:bold; color:white">PhotoCom</p></center>
+    </div>
+    
+    <div class="container"><br><br>
+    <p style="font-size:20px">
+    PhotoCom provides a way to connect people with professional photographers and photographers to assistants. 
+    Photographers and assistants create personal profiles containing their portfolios, job history, location, 
+    rates, and to manage availability. This information is utilized to return relevant lists of professionals 
+    who have the skills and availability to complete jobs entered by users.<br><br> 
+ 
+    PhotoCom facilitates communication allowing for quick discussion and negotiation. Photographers and assistants 
+    can accept jobs which are then added to their calendars.  They can view and manage jobs they’ve accepted 
+    including viewing information about the job itself and request third party information such as weather forecasts 
+    and directions. Users also have the option to cancel scheduled jobs. Users have the option to rate work once it’s 
+    completed and write reviews which can be showcased on photographers’ and assistants’ profiles.
+    </p></div>
+
+    
+</html>
